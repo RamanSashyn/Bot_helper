48b131c85061f6163f20a94af49cdd4ce852caf3
Raman <romansashin04@gmail.com>
2025-06-19 14:54:59 +0300

    Ехо бот для ВК

--- vk_bot.py
+++ vk_bot.py
@@ -1,19 +1,26 @@
 import os
 import vk_api
+import random
 from vk_api.longpoll import VkLongPoll, VkEventType
 from dotenv import load_dotenv
 
-load_dotenv()
 
-VK_API_TOKEN = os.getenv("VK_API_TOKEN")
-vk_session = vk_api.VkApi(token=VK_API_TOKEN)
-longpoll = VkLongPoll(vk_session, preload_messages=True)
+def echo(event, vk_api):
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=event.text,
+        random_id=random.randint(1, 1000)
+    )
 
-for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW:
-        print('Новое сообщение:')
-        if event.to_me:
-            print('Для меня от: ', event.user_id)
-        else:
-            print('От меня для: ', event.user_id)
-        print('Текст:', event.text)
+
+if __name__ == "__main__":
+    load_dotenv()
+    VK_API_TOKEN = os.getenv("VK_API_TOKEN")
+    vk_session = vk_api.VkApi(token=VK_API_TOKEN)
+    vk_api = vk_session.get_api()
+    longpoll = VkLongPoll(vk_session, preload_messages=True)
+
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            echo(event, vk_api)
+
