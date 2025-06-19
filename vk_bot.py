1f086b74c7f1943e7fd506a51c1eaf34b5db32d0
Raman <romansashin04@gmail.com>
2025-06-19 15:13:54 +0300

    Бот не отвечает если отправлено не понятное сообщение

--- vk_bot.py
+++ vk_bot.py
@@ -1,9 +1,10 @@
 import os
-import vk_api
 import random
-from google.cloud import dialogflow_v2 as dialogflow
-from vk_api.longpoll import VkLongPoll, VkEventType
+
 from dotenv import load_dotenv
+from google.cloud import dialogflow_v2 as dialogflow
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
 
 
 def detect_intent_texts(project_id, session_id, text, language_code="ru"):
@@ -17,31 +18,32 @@ def detect_intent_texts(project_id, session_id, text, language_code="ru"):
         request={"session": session, "query_input": query_input}
     )
 
-    return response.query_result.fulfillment_text
+    return response.query_result
 
 
 if __name__ == "__main__":
     load_dotenv()
+
     VK_API_TOKEN = os.getenv("VK_API_TOKEN")
     PROJECT_ID = os.getenv("PROJECT_ID")
 
     vk_session = vk_api.VkApi(token=VK_API_TOKEN)
-    vk_api = vk_session.get_api()
+    vk_api_instance = vk_session.get_api()
     longpoll = VkLongPoll(vk_session, preload_messages=True)
 
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            response_text = detect_intent_texts(
+            result = detect_intent_texts(
                 project_id=PROJECT_ID,
                 session_id=event.user_id,
                 text=event.text
             )
 
-            if not response_text:
-                response_text = "Простите, я не понял вопрос."
+            if result.intent.is_fallback:
+                continue
 
-            vk_api.messages.send(
+            vk_api_instance.messages.send(
                 user_id=event.user_id,
-                message=response_text,
+                message=result.fulfillment_text,
                 random_id=random.randint(1, 1000),
             )
